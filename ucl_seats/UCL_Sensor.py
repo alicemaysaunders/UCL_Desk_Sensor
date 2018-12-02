9946862a72741512eae1067ec03884c2ec5c7146
Alice Saunders <alicemaysaunders@gmail.com>
2018-12-02 19:38:03 +0000

    This finds the map id and survey pairs and uses them to display a map, but still looks a bit weird

--- ucl_seats/UCL_Sensor.py
+++ ucl_seats/UCL_Sensor.py
@@ -1,12 +1,13 @@
 from flask import Flask, render_template, request #use python in HTML 
 import requests #Talks to API
 
-params = {
+librarys_list_params = {
   "token": "uclapi-738dd8c9e77498a-39aefce60ca1db6-50fe1ceb32dc5d4-c03b8b4218e13a8",
 } #my UCL API token
-r = requests.get("https://uclapi.com/workspaces/surveys", params=params)
+librarys_list_response = requests.get("https://uclapi.com/workspaces/surveys", params=librarys_list_params)
 
-librarys = r.json()['surveys'] #gets dictionary of UCL librarys 
+librarys = librarys_list_response.json()['surveys'] #gets dictionary of UCL librarys in .json format
+
 
 app = Flask("MyApp")
 
@@ -14,15 +15,19 @@ app = Flask("MyApp")
 def hello():
     return render_template("UCL_seats.html", librarys=librarys) #use my HTML file in browser and define Librarys in HTML doc in the same way as in my Python doc
 
-@app.route("/library/<id>") #When someone goes to homepage/id, run the function below
-def library_page(id):
-	params = {
+@app.route("/library/<library_id>/<map_id>") #When someone goes to homepage/id, run the function below
+def library_page(library_id,map_id): #pass these arguments through the function every time someone goes to this page 
+	map_image_params = {
 	  "token": "uclapi-738dd8c9e77498a-39aefce60ca1db6-50fe1ceb32dc5d4-c03b8b4218e13a8",
-	  "survey_id": 22,
-	  "map_id": 3
+	  "survey_id": library_id,
+	  "map_id": map_id
 	}
 
-	r = requests.get("https://uclapi.com/workspaces/images/map/live", params=params)
-	print(r.json())
+	map_image_response = requests.get("https://uclapi.com/workspaces/images/map/live", params=map_image_params) #requests documentation 
+	
+	 
+	map_image = map_image_response.text
+	return render_template("mapimage.html", map_image= map_image) 
+
 
 app.run(debug=True) 
