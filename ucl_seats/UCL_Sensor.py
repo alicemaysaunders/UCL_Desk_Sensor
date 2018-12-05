7f63af76ae26ef4f8a9bdbfb4411a09ab6d8c831
juliakrol8 <julia.krol@hotmail.com>
2018-12-05 20:40:40 +0000

    Image fix
    Tried to fix the images however I think there is a general problem with
    the API (html/css update coming later :) )

--- ucl_seats/UCL_Sensor.py
+++ ucl_seats/UCL_Sensor.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request #use python in HTML 
+from flask import Flask, render_template, request #use python in HTML
 import requests #Talks to API
 
 librarys_list_params = {
@@ -11,23 +11,25 @@ librarys = librarys_list_response.json()['surveys'] #gets dictionary of UCL libr
 
 app = Flask("MyApp")
 
-@app.route("/") #When someone goes to the homepage, run the function below 
+@app.route("/") #When someone goes to the homepage, run the function below
 def hello():
-    return render_template("UCL_seats.html", librarys=librarys) #use my HTML file in browser and define Librarys in HTML doc in the same way as in my Python doc
+	return render_template("UCL_seats.html", librarys=librarys) #use my HTML file in browser and define Librarys in HTML doc in the same way as in my Python doc
 
 @app.route("/library/<library_id>/<map_id>") #When someone goes to homepage/id, run the function below
-def library_page(library_id,map_id): #pass these arguments through the function every time someone goes to this page 
+def library_page(library_id,map_id): #pass these arguments through the function every time someone goes to this page
 	map_image_params = {
 	  "token": "uclapi-738dd8c9e77498a-39aefce60ca1db6-50fe1ceb32dc5d4-c03b8b4218e13a8",
 	  "survey_id": library_id,
-	  "map_id": map_id
+	  "map_id": map_id,
+      "image_scale": 0.015
 	}
 
-	map_image_response = requests.get("https://uclapi.com/workspaces/images/map/live", params=map_image_params) #requests documentation 
-	
-	 
-	map_image = map_image_response.text
-	return render_template("mapimage.html", map_image= map_image) 
+	map_image_response = requests.get("https://uclapi.com/workspaces/images/map/live", params=map_image_params) #requests documentation
 
 
-app.run(debug=True) 
+	map_image_text = map_image_response.text
+	map_image = map_image_text.encode('base64')
+	return render_template("mapimage.html", map_image= map_image)
+
+
+app.run(debug=True)
